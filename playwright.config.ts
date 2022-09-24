e094423fb6ca3a330e6d5bd750cc1e8aec725bdc
James Camilleri <james@james.mt>
2022-09-24 14:12:49 +0100

    Update configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,16 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
+const PORT = 4173;
+
 const config: PlaywrightTestConfig = {
+	timeout: 5 * 60 * 1000,
+	fullyParallel: true,
+	use: {
+		baseURL: `http://localhost:${PORT}`
+	},
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: PORT
 	}
 };
 
